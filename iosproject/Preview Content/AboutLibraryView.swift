750900c60706d7d8f72d94642b5d99567a92e3c7
Subash Gaddam <subashgaddam@Subashs-Laptop.local>
2024-08-06 23:55:35 -0400

    working on about library page modifications

--- iosproject/Preview Content/AboutLibraryView.swift
+++ iosproject/Preview Content/AboutLibraryView.swift
@@ -13,25 +13,29 @@ struct AboutLibraryView: View {
             Text("About Library")
                 .font(.largeTitle)
                 .fontWeight(.bold)
-                .padding(10)
+                .padding(10).background(.gray)
 
             Text("library offer a wide range of books from various genres and authors. Our mission is to foster a love of reading and provide a community space for learning and growth.")
+                .foregroundColor(.gray)
 
             Text("Library Hours")
                 .font(.headline)
                 .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+               
 
             Text("""
                 Monday - Friday: 9 AM - 6 PM
                 Saturday: 10 AM - 4 PM
                 Sunday: Closed
                 """)
+            .foregroundColor(.gray)
 
             Text("Location")
                 .font(.headline)
                 .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
 
             Text("5290 Chem. de la Côte-des-Neiges, Montréal, QC H3T 1Y2")
+                .foregroundColor(.gray)
 
             Spacer()
         }
